07eb2517ca5f7b91fe20814e7bc604e650265f04
brayan <brayan.mail.tester@gmail.com>
2023-06-13 18:59:53 -0600

    sdsd

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:brayan-web/portafolio-git.git master:gh-pages
+git push -f git@github.com:brayan-web/portafolio-web.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: '/portafolio-git',
+  publicPath: '/portafolio-web',
 };
